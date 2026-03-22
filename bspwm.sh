1ab1cb6f4d2feca87343e22080f8c7f11838b2db
linux <linux@Debian.ipv6.infinitum.net.mx>
2026-03-22 06:53:04 +0000

    se agrego el framework oh my zsh / powerlevel10k como tambien algunos programas

--- bspwm.sh
+++ bspwm.sh
@@ -7,7 +7,7 @@ RESET='\e[0m'
 
 #Instalacion del sevidor grafico y algunas herramientaas. =========================
 echo -e "${VERDE} Instalando servidor grafico, modulos de bluetooh y brillo${RESET}"
-GRAPHIC_SETUP="xorg bspwm polybar picom zsh gzip kitty rofi firefox-esr feh git curl wget lightdm light-locker"
+GRAPHIC_SETUP="xorg bspwm polybar picom zsh gzip kitty rofi firefox-esr feh git curl wget lightdm light-locker btop htop cmatrix"
 #Ejecucion Limpia
 sudo apt install $GRAPHIC_SETUP -y
 
@@ -39,3 +39,19 @@ echo -e "${VERDE}Configuraciòn completada con èxito.${RESET}"
 echo -e "${VERDE} otorgando permisos de ejecucion para bspwmrc y sxhkcrc"
 chmod +x "$HOME_DIR/.config/bspwm/bspwmrc"
 chmod +x "$HOME_DIR/.config/sxhkd/sxhkdrc"
+
+# --- Instalación de oh my zsh y Powerlevel10k ---
+echo "${VERDE}Instalando oh my zsh y temas...${RESET}"
+
+#Clonar Oh My Zsh (opcional pero recomendado para plugins)
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+#Clonar Powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+#Configurar Zsh como shell por defecto (esto pedirá contraseña al usuario)
+chsh -s $(which zsh)
+
+echo -e "${VERDE}Instalación de Zsh completada. Al reiniciar la terminal, se ejecutará el asistente de p10k.${RESET}"
+
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel10k\/powerlevel10k"/' ~/.zshrc
